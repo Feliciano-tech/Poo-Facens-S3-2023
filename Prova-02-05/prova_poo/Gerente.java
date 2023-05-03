426627c93a7919fb2057bd3264f652d812d4d113
Matheus Feliciano do Nascimento <104037126+Feliciano-tech@users.noreply.github.com>
2023-05-02 22:22:57 -0300

    Add files via upload

---
+++ Prova-02-05/prova_poo/Gerente.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prova_poo;
+
+/**
+ *
+ * @author aluno
+ */
+public class Gerente extends Funcionario {
+
+    private String senha;
+
+    public Gerente() {
+    }
+
+    public Gerente(String senha) {
+        this.senha = senha;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String cadastrarSenha(String senha) {
+
+        if (senha.length() <= 5) {
+
+            return "Senha deve conter no minimo 5 caracteres";
+
+        } else {
+            return "Senha cadastrada com sucesso!!!";
+
+        }
+
+    }
+
+    public boolean autenticarSenha(String senha) {
+
+        if (senha.equals(getSenha())) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void setBonificacao() {
+        double salario = 0;
+        salario += salario * 0.2;
+
+    }
+
+}
---
+++ Prova-02-05/prova_poo/Main.java
@@ -0,0 +1,28 @@
+
+package prova_poo;
+
+
+public class Main {
+
+    
+    public static void main(String[] args) {
+        
+        Funcionario p1 = new Funcionario();
+        
+        p1.setSalario(1000);
+        p1.setBonificacao();
+        
+        System.out.println(p1.getSalario());
+        
+        
+        
+        
+    }
+    
+    
+    
+
+   
+    
+    
+}
